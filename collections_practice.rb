a4cad02939e6759572a64020a6f6c93c0cf43886
Janine Luk <janineluk95@gmail.com>
2018-11-15 10:00:50 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,6 +2,6 @@ def begins_with_r(tools)
   tools.all? {|tool| tool.start_with?("r")}
 end
 
-def contain_a(elements)
-  elements.all? {|element| element.include?("a")}
+def contain_a(array)
+  array.all? {|element| element.include?("a")}
 end
