a4202d798b457e021c9252134ceb359bbed9bee4
Janine Luk <janineluk95@gmail.com>
2018-11-15 09:30:41 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,7 +2,7 @@ def begins_with_r(tools)
   i = 0 
   new_tools = []
   while i < tools.length
-    if tools.start_with?(r)
+    if tools.start_with?("r")
     return true
   else 
     return false 
